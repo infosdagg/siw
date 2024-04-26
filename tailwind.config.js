4f8cfd75ca77bba150d8501483be8ab51435fe64
infosdagg <168018272+infosdagg@users.noreply.github.com>
2024-04-26 18:17:02 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors : {
+      navPurple : "#E7DEF4",
+      iconColor : "#5300BC",
+      inputColor : "#5300BC1A",
+      white : "#fff",
+      buttonHover : "#7C3ECB",
+      textWhite : "#f6f6f6",
+      green : "#A5F6F0",
+      pink : "#E7DEF4",
+      btnText : "#CDB9E8",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
